ee32f351200434d61e0e04e2d7b859824382e339
Lucien Carrer <carrer.lucien@gmail.com>
2024-03-15 16:41:12 +0100

    correcting typos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Menu from "/src/components/menu/Menu";
-import FadeInMount from "/src/components/FadeInMount/FadeInMount";
+import FadeInMount from "/src/components/fadeInMount/FadeInMount";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
